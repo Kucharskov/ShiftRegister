95fade40b3ea3eb87aeddf0a9d5da1b4e6283916
Michał Kucharski <M.Kucharskov@onet.pl>
2019-02-25 20:47:34 +0100

    Upload library

---
+++ src/ShiftRegister.cpp
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+#include "ShiftRegister.h"
+
+bool ShiftRegister::isCorrectPin(unsigned int pin) {
+  return pin < length;
+}
+
+ShiftRegister::ShiftRegister(unsigned int data, unsigned int clk, unsigned int latch) :
+  dataPin(data),
+  clockPin(clk),
+  latchPin(latch),
+  autoUpdate(false),
+  length(8) {
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+  regArray = new bool[length];
+};
+
+ShiftRegister::~ShiftRegister() {
+  delete[] regArray;
+}
+
+void ShiftRegister::update() {
+  digitalWrite(latchPin, false); 
+  for(int i = length-1; i >= 0; i--){
+  	digitalWrite(clockPin, false);
+  	digitalWrite(dataPin, regArray[i]);
+  	digitalWrite(clockPin, true); 
+  }
+  digitalWrite(latchPin, true); 
+}
+
+void ShiftRegister::clear() {
+  for(int i = 0; i < length; i++) {
+    regArray[i] = false;
+  }
+
+  if(autoUpdate) update();
+}
+
+void ShiftRegister::push(bool bit) {
+  for(int i = length-1; i > 0; i--) {
+    regArray[i] = regArray[i - 1];
+  }
+  regArray[0] = bit;
+  
+  if(autoUpdate) update();
+}
+
+void ShiftRegister::invert() {
+  for(int i = 0; i < length; i++) {
+    regArray[i] = !regArray[i];
+  }
+  
+  if(autoUpdate) update();
+}
+
+void ShiftRegister::reverse() {
+  for(int i = 0; i < length/2; i++) {
+	  bool temp = regArray[i];
+	  regArray[i] = regArray[length - 1 - i];
+	  regArray[length - 1 - i] = temp;
+  }
+  
+  if(autoUpdate) update();
+}
+
+void ShiftRegister::setAutoUpdate(bool mode) {
+  autoUpdate = mode;
+}
+
+void ShiftRegister::setLength(unsigned int newLength) {
+  if(newLength <= 0 || newLength == length) return;
+  
+  delete[] regArray;
+  
+  length = newLength;
+  regArray = new bool[length];
+  
+  if(autoUpdate) update();
+}
+
+unsigned int ShiftRegister::getLength() {
+  return length;
+}
+
+void ShiftRegister::setPin(unsigned int pin, bool value) {
+  if(!isCorrectPin(pin)) return;   
+	 
+  regArray[pin] = value;
+  
+  if(autoUpdate) update();
+}
+
+bool ShiftRegister::getPin(unsigned int pin) {
+  if(!isCorrectPin(pin)) return false;   
+  
+  return regArray[pin];
+}
+
+void ShiftRegister::invertPin(unsigned int pin) {
+  if(!isCorrectPin(pin)) return; 
+	   
+  regArray[pin] = !regArray[pin];
+  
+  if(autoUpdate) update();
+}
+
+void ShiftRegister::loadData(unsigned int data) {
+  for (int i = 0; i < length; i++) {
+	  regArray[i] = ((data >> i) & 1) == 1;
+  }
+  
+  if(autoUpdate) update();
+}
+
+void ShiftRegister::loadData(bool * data, unsigned int length) {
+  int size = min(length, this->length);
+  for(int i = 0; i < size; i++) {
+	  regArray[size - 1 - i] = data[i];
+  }
+  
+  if(autoUpdate) update();
+}
---
+++ src/ShiftRegister.h
@@ -0,0 +1,54 @@
+/*
+  ShiftRegister.h - Library for operating ShiftRegister (eg. 74HC595)
+  Created with love by M. Kucharskov (https://kucharskov.pl)
+*/
+ 
+#ifndef ShiftRegister_h
+#define ShiftRegister_h
+ 
+#include <Arduino.h>
+
+class ShiftRegister {
+  private:
+    //Pins
+    unsigned int latchPin;
+    unsigned int dataPin;
+    unsigned int clockPin;
+
+    //Additional stuff
+    bool autoUpdate;
+
+    //Array things
+    unsigned int length;
+    bool * regArray;
+
+    //Security check
+    bool isCorrectPin(unsigned int pin);
+
+  public:
+  	ShiftRegister(unsigned int data, unsigned int clk, unsigned int latch);
+  	~ShiftRegister();
+   
+  	void update();
+    void clear();
+    void push(bool bit);
+    void invert();
+    void reverse();
+
+    void setAutoUpdate(bool mode);
+  	void setLength(unsigned int newLength);
+  	unsigned int getLength();
+    
+  	void setPin(unsigned int pin, bool value);
+    bool getPin(unsigned int pin);
+  	void invertPin(unsigned int pin);
+    
+  	void loadData(unsigned int data);
+  	void loadData(bool * data, unsigned int length);
+};
+
+#endif
+
+//Digispark Fix
+//void * operator new[] (size_t size) { return malloc(size); }
+//void operator delete[] (void * ptr) { free (ptr); }
